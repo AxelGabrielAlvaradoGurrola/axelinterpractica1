20c3c905988e2d0fe68ab16bc7e18305ef5f4dcc
AxelGabrielAlvaradoGurrola <108039436+AxelGabrielAlvaradoGurrola@users.noreply.github.com>
2022-08-19 12:00:30 -0500

    Add files via upload

---
+++ G4s21axel4295e3/src/Procesos/OperacionesAvanzadas.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Procesos;
+
+/**
+ *
+ * @author axelgabrielalvaradogurrola
+ */
+public class OperacionesAvanzadas {
+    
+    public double PotenciaN(int base, int potencia){
+        return (Math.pow(base, potencia));
+    }
+    //crear funciones de raiz cuadrada, raiz cubica, factorial, logaritmo
+}
